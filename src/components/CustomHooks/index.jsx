ad743a0043490180c4fc7ea02eabda0ec4ccc50c
Leonardo Nicola <leonardonicolares@gmail.com>
2022-09-20 17:56:53 -0300

    criado um custom hook que faz um Fetch e detalha por ID clicado

---
+++ src/components/CustomHooks/index.jsx
@@ -0,0 +1,105 @@
+import { useEffect, useRef, useState } from 'react';
+
+const isObjectEqual = (objA, objB) => {
+  return JSON.stringify(objA) === JSON.stringify(objB);
+};
+
+const useFetch = (url, options) => {
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [shouldLoad, setShouldLoad] = useState(false);
+  const urlRef = useRef(url);
+  const optionsRef = useRef(options);
+
+  useEffect(() => {
+    let changed = false;
+
+    if (!isObjectEqual(url, urlRef.current)) {
+      urlRef.current = url;
+      changed = true;
+    }
+
+    if (!isObjectEqual(options, optionsRef.current)) {
+      optionsRef.current = options;
+      changed = true;
+    }
+
+    if (changed) {
+      setShouldLoad((s) => !s);
+    }
+  }, [url, options]);
+
+  useEffect(() => {
+    let wait = false;
+    console.log('EFFECT', new Date().toLocaleString());
+
+    setLoading(true);
+
+    const fetchData = async () => {
+      await new Promise((r) => setTimeout(r, 1000));
+
+      try {
+        const response = await fetch(urlRef.current, optionsRef.current)
+        .then((res) => res.json());
+
+        if (!wait) {
+          setResult(response);
+          setLoading(false);
+        }
+      } catch (e) {
+        if (!wait) {
+          setLoading(false);
+        }
+        throw e;
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      wait = true;
+    };
+  }, [shouldLoad]);
+
+  return [result, loading];
+};
+
+const Home = () => {
+  const [postId, setPostId] = useState('');
+  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId);
+
+  useEffect(() => {
+    console.log('ID do post', postId);
+  }, [postId]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  const handleClick = (id) => {
+    setPostId(id);
+  };
+
+  if (!loading && result) {
+    // 1
+    return (
+      <div>
+        {result?.length > 0 ? (
+          result.map((p) => (
+            <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
+              <p>{p.title}</p>
+            </div>
+          ))
+        ) : (
+          <div onClick={() => handleClick('')}>
+            <p>{result.title}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  return <h1>Oi</h1>;
+};
+
+export default Home;
