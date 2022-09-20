752fda43e12a220dee4760c4b67309e259d819e6
Leonardo Nicola <leonardonicolares@gmail.com>
2022-09-20 17:57:19 -0300

    modif

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
-/* import UseCallback from './components/UseCallback'; */
-/* import UseEffect from './components/UseEffect'; */
-/* import UseMemoHook from './components/UseMemoHook' */
-/* import UseContextHook from './components/UseContextHook' */
-import UseReducerHook from './components/UseReducerHook';
+import CustomHooks from './components/CustomHooks'
 function App() {
   return (
     <>
-      {/* <UseEffect /> */}
-      {/* <UseCallback/> */}
-      {/* <UseMemoHook/> */}
-      {/* <UseContextHook /> */}
-      {<UseReducerHook/>}
+        <CustomHooks />
     </>
   );
 }
