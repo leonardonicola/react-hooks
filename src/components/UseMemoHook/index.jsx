67c8401964339518560f83813f6605955f8a707e
Leonardo Nicola <leonardonicolares@gmail.com>
2022-09-20 14:42:28 -0300

    useMemo hook explicado

--- src/components/UseMemoHook/index.jsx
+++ src/components/UseMemoHook/index.jsx
@@ -1,25 +1,37 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import './styles.css';
-import Post from './Post';
+import Post from '../../utils/Post';
+import loadPosts from '../../utils/loadPosts';
+import Button from '../../utils/Button';
 
 const UseMemoHook = () => {
-  console.log('PARENT - renderizou');
   const [posts, setPosts] = useState([]);
   const [searchName, setValue] = useState('');
+  const [counter, setCounter] = useState(10);
+
+  const handleIncrement = (qty) => {
+    setCounter((prev) => (prev += qty));
+  };
 
   //componentDidMount - executado apenas 1x
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => res.json())
-      .then((res) => setPosts(res));
-  }, []);
+    const handleLoadPosts = async () => {
+      const data = await loadPosts(counter);
+      setPosts(data);
+    };
+    handleLoadPosts();
+  }, [counter]);
 
+  console.log('PARENT - renderizou');
   return (
     <div className="main">
       <input type="search" value={searchName} onChange={(e) => setValue(e.target.value)} />
+      {useMemo(() => {
+        return <Button increment={handleIncrement} />;
+      }, [])}
       <div className="container">
         {/* quando useMemo é usado, o component Post é renderizado apenas uma vez, visto
-        que é apenas um component funcional.
+        que é apenas um functional component.
         já o component parent (esse aqui), será renderizado toda vez que houver alteração de state.*/}
         {useMemo(() => {
           return posts.length > 0 && posts.map((post) => <Post key={post.id} post={post} />);
