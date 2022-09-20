094303cb8d18e322ba0e3a7478f0e0f70a32d500
Leonardo Nicola <leonardonicolares@gmail.com>
2022-09-20 14:45:41 -0300

    botão utilizado para exemplos

--- src/utils/button.js
+++ src/utils/button.js
@@ -3,7 +3,7 @@ import P from 'prop-types';
 const Button = ({ increment }) => {
   console.log('BUTTON - renderizei');
 
-  return <button onClick={() => increment(10)}>PLUS 10</button>;
+  return <button onClick={() => increment(5)}>LOAD MORE</button>;
 };
 
 Button.propTypes = {
