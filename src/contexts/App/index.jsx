6bf9f1e842fc2739ea1223032c7e111c4339d843
Leonardo Nicola <leonardonicolares@gmail.com>
2022-09-23 14:49:39 -0300

    useContext studies

--- src/contexts/App/index.jsx
+++ src/contexts/App/index.jsx
@@ -1,13 +1,16 @@
-import {createContext} from 'react';
-import P from 'prop-types'
+import { createContext } from 'react';
+import P from 'prop-types';
 import globalState from './data';
 
 export const GlobalContext = createContext();
+
+/* GlobalContext vira um component que recebe um estado
+(na flag Provider) que pode ser utilizado por qualquer componente. */
 
 export const AppContext = ({ children }) => {
   return <GlobalContext.Provider value={globalState}>{children}</GlobalContext.Provider>;
 };
 
 AppContext.propTypes = {
-  children: P.node
-}
+  children: P.node,
+};
---
+++ src/template/App/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css';
+import UseContextHook from '../../components/UseContextHook'
+function App() {
+  return (
+    <>
+        <UseContextHook />
+    </>
+  );
+}
+
+export default App;
